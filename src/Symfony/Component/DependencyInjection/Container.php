4e267ed5bb4f43abd5318d65b5549deee3af7bda
7x <info@se7enx.com>
2026-02-10 20:03:39 -0800

    Update: PHP85 Compatiblity bugfixes for deprecation warnings. Tested. Bugfixes.

--- src/Symfony/Component/DependencyInjection/Container.php
+++ src/Symfony/Component/DependencyInjection/Container.php
@@ -63,7 +63,7 @@ class Container implements ResettableContainerInterface
     private $compiled = false;
     private $getEnv;
 
-    public function __construct(ParameterBagInterface $parameterBag = null)
+    public function __construct(?ParameterBagInterface $parameterBag = null)
     {
         $this->parameterBag = $parameterBag ?: new EnvPlaceholderParameterBag();
     }
